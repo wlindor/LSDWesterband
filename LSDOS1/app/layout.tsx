a7d18d699530a544bd4bade139de5c4de90edf65
ROAI <developer@roai-research.net>
2024-06-30 03:56:53 +0000

    okay

--- LSDOS1/app/layout.tsx
+++ LSDOS1/app/layout.tsx
@@ -1,13 +1,19 @@
 // app/layout.tsx
-import './globals.css';
+import '../app/styles/globals.css';
 import { ReactNode } from 'react';
 import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from '@clerk/nextjs';
 import { cn } from './lib/utils';
+import { Inter as FontSans } from "next/font/google"
 import { AuthProvider } from './AuthContext';
 
 interface LayoutProps {
   children: ReactNode;
 }
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export default function RootLayout({ children }: LayoutProps) {
   const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
@@ -22,7 +28,10 @@ export default function RootLayout({ children }: LayoutProps) {
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </head>
-        <body className={cn('min-h-screen bg-gray-100 dark:bg-gray-900 font-sans antialiased')}>
+        <body
+          className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable)}>
           <AuthProvider>
             <div className="flex flex-col min-h-screen">
               <main className="flex-grow">
