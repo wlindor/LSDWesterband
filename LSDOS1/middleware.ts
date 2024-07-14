6f5c06f75416c880bb489851daf41e86b85a8861
wlindor <47373982+wlindor@users.noreply.github.com>
2024-07-13 23:21:11 -0400

    updating

--- LSDOS1/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware({
-    publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-  });
-  
-export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
